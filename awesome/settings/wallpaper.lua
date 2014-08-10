2667c03584eab0f3cf992de369e25cdf1f621c00
Jishnu Mohan <jishnu7@gmail.com>
2014-08-10 19:21:54 +0530

    random wallpaper on login

---
+++ awesome/settings/wallpaper.lua
@@ -0,0 +1,36 @@
+-- scan directory, and optionally filter outputs
+function scandir(directory, filter)
+    local i, t, popen = 0, {}, io.popen
+    if not filter then
+        filter = function(s) return true end
+    end
+    print(filter)
+    for filename in popen('ls -a "'..directory..'"'):lines() do
+        if filter(filename) then
+            i = i + 1
+            t[i] = filename
+        end
+    end
+    return t
+end
+
+-- }}}
+-- Random wallpaper
+wp_index = 1
+wp_timeout  = 10
+wp_path = "/media/data/pictures/wallpapers/"
+wp_filter = function(s) return string.match(s,"%.png$") or string.match(s,"%.jpg$") end
+wp_files = scandir(wp_path, wp_filter)
+
+function random_wallpaper()
+
+  wp_index = math.random( 1, #wp_files)
+  -- set wallpaper to current index for all screens
+  for s = 1, screen.count() do
+    gears.wallpaper.maximized(wp_path .. wp_files[wp_index], s, true)
+  end
+
+end
+
+random_wallpaper()
+-- }}}
