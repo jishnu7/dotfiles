e2db45679204226b8918bdf3656f56c9ab7057ee
Jishnu Mohan <jishnu7@gmail.com>
2019-10-24 16:59:23 +0530

    lock screen image

--- i3/scripts/lock.sh
+++ i3/scripts/lock.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
 
 # Take a screenshot
-scrot /tmp/screen_locked.png
+#scrot /tmp/screen_locked.png
 
 # Pixellate it 10x
-mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
+#mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
 
 # Lock screen displaying this image.
-i3lock -i /tmp/screen_locked.png
+#i3lock -i /tmp/screen_locked.png
+i3lock -i /home/jishnu/Pictures/this_one_flies.png
 
 # Turn the screen off after a delay.
 sleep 60; pgrep i3lock && xset dpms force off
