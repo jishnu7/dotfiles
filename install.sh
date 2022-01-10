f3a9b7bdd108ca41cf48d90ea373782ab2e4fd07
Jishnu Mohan <jishnu7@gmail.com>
2022-01-10 18:20:06 +0530

    more brew packages, use fzf directly in vim

--- install.sh
+++ install.sh
@@ -73,5 +73,5 @@ ln -s $PWD/dircolors/dircolors.ansi-dark ~/.dircolors
 touch ~/.hushlogin
 # Packages
 brew install --cask iterm2 numi keyman dozer rectangle libreoffice
-brew install tmux wget
+brew install tmux wget htop tree
 brew install --cask homebrew/cask-fonts/font-inconsolata-for-powerline
